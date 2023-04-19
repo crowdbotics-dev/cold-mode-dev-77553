ea9be7316e38ce83a0fc8ab271d8e506f64a495e
Crowdbotics <engineering@crowdbotics.com>
2023-04-19 05:52:43 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/cold_mode_dev_77553/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cold_mode_dev_77553;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "cold_mode_dev_77553";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "cold-mode-dev-77553",
+  url: "https://cold-mode-dev-77553.botics.co",
+  api: "https://cold-mode-dev-77553.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
